090ff76e5a7c9c08dab28c13b38d4c04182ebe3f
Bruno Castro <104720539+BrunoCastroM@users.noreply.github.com>
2022-08-08 10:28:02 -0300

    back to the project

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('HOME 2')
+    return HttpResponse('''<!DOCTYPE>
+    <html>
+    <head><title>Hello world</title></head>
+    <body>
+        <h1>Hello world!</h1> 
+    </body>
+    </html>
+    ''')
 
 
 def contato(request):
